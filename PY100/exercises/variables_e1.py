55f8c2aede94212c4e99a97de550a93a7851446e
Jose Alexander Penado <j.penado@outlook.com>
2025-08-22 15:05:38 -0500

    Add variables exercise #1

---
+++ PY100/exercises/variables_e1.py
@@ -0,0 +1,16 @@
+# Classify the following potential non-constant
+# variable names as idiomatic, non-idiomatic, or
+# illegal. For the non-idiomatic and illegal names,
+# explain your choice.
+
+"""
+Name			Choice				Explanation
+index			idiomatic
+CatName			non-idiomatic		The name does not follow variable name styling. Capital C & N
+lazy_dog		idiomatic
+quick_Fox		non-idiomatic		The name has a capital F
+1stCharacter	illegal				The name starts with a number in this case 1
+operand2		idiomatic
+BIG_NUMBER		non-idiomatic		All capitals should be used for non-constant variable names
+π				non-idiomatic		A symbol should be used for non-constant variable names
+"""
