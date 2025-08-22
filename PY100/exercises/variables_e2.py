5fefd189be7c8cf5726a94eaf0ad0b1138fca45d
Jose Alexander Penado <j.penado@outlook.com>
2025-08-22 15:17:41 -0500

    Add answer to 2nd variables exercise

---
+++ PY100/exercises/variables_e2.py
@@ -0,0 +1,14 @@
+# Classify the following potential function names
+# as idiomatic, non-idiomatic, or illegal. For the
+# non-idiomatic and illegal names, explain your choice
+
+"""
+index			idiomatic
+CatName			non-idiomatic			Upper case letters and no underscore between words
+lazy_dog		idiotmatic
+quick_Fox		non-idiomatic			Upper case letter
+1stCharacter	illegal					Number used as 1st letter of name
+operand2		idiomatic
+BIG_NUMBER		non-idiomatic			Uppercase letters
+π				non-idiomatic			lower case, underscore, and digits 0 - 9 only
+"""
