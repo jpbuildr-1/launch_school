02eae8dd6ddb2835f533f9950ebe6a2fb5c10fe6
Jose Alexander Penado <j.penado@outlook.com>
2025-08-22 15:28:09 -0500

    Add answers to 3rd variables exercise

---
+++ PY100/exercises/variables_e3.py
@@ -0,0 +1,15 @@
+# Classify the following potential constant
+# names as idiomatic, non-idiomatic, or illegal
+# For the non-idiomatic and illegal names,
+# explain your choice.
+
+"""
+index			non-idiomatic			letters are not all uppercase
+CatName			non-idiomatic			letters are not all uppercase
+snake_case		non-idiomatic			letter are not all uppercase
+LAZY_DOG3		idiomatic
+1ST				illegal					name starts with a number
+operand2		non-idiomatic			letters are not uppercase
+BIG_NUMBER		idiomatic
+Π				non-idiomatic			names should only use standard ASCII characters
+"""
