8c3727d246e3676ec53fc15e8865e662cdcbf155
Jose Alexander Penado <j.penado@outlook.com>
2025-08-22 16:25:18 -0500

    Add answers to variables exercise 7.

---
+++ PY100/exercises/variables_e7.py
@@ -0,0 +1,14 @@
+# What happens when you run the following code? Why?
+
+NAME = 'Victor'
+print('Good Morning, ' + NAME) # Good Morning, Victor
+print('Good Afternoon, ' + NAME) # Good Afternoon, Victor
+print('Good Evening, ' + NAME) # Good Evening, Victor
+
+# this would print
+# Good
+
+NAME = 'Nina'
+print('Good Morning, ' + NAME) # Good Morning, Nina
+print('Good Afternoon, ' + NAME) # Good Afternoon, Nina
+print('Good Evening, ' + NAME) # Good Evening, Nina
