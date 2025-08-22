330588efac58e7b1074036071be48ea154345d2b
Jose Alexander Penado <j.penado@outlook.com>
2025-08-22 15:34:46 -0500

    Add answers to variables exercise #4

---
+++ PY100/exercises/variables_e4.py
@@ -0,0 +1,14 @@
+# Classify the following potential class names
+# as idiomatic, non-idiomatic, or illegal.
+# For the non-idiomatic and illegal names,
+# explain your choice.
+
+"""
+index			non-idiomatic			first letter is not upper case
+CatName			idiomatic
+Lazy_Dog		non-idiomatic			name includes an underscore
+1ST				illegal					name starts with a number
+operand2		non-idiomatic			first letter is not uppercased
+BigNumber3		idiomatic
+Πi				non-idiomatic			name should be standard ASCII characters
+"""
