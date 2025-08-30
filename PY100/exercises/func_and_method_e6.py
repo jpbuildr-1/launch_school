0ee5acd75af98cec74c5fca34e3192d12e9c7ebd
Jose Alexander Penado <j.penado@outlook.com>
2025-08-30 18:12:26 -0500

    Add answer to Functions and Methods exercise #6

---
+++ PY100/exercises/func_and_method_e6.py
@@ -0,0 +1,8 @@
+# What does the following code print?
+
+def scream(words):
+	words = words + '!!!!'
+	return
+	print(words)
+
+scream('Yipeee') # Does not print anything. When the code is invoked and it gets to return the function stops
