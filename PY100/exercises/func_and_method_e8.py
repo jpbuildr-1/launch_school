9170a619c09b3ee29ae4b826516927990e160709
Jose Alexander Penado <j.penado@outlook.com>
2025-08-30 18:17:50 -0500

    Add answer to functions and methods exercise #8.py

---
+++ PY100/exercises/func_and_method_e8.py
@@ -0,0 +1,6 @@
+# Without running the following code, what do you think it will do?
+def foo(bar, qux):
+	print(bar)
+	print(qux)
+
+foo(42, 3.141592, 2.718) # This will return an error because you are passing an extra argument when you should not be.
