7b6dc0e7a30b87a8edcd534bbe540411f9e04d24
Jose Alexander Penado <j.penado@outlook.com>
2025-08-30 18:15:06 -0500

    Add answer to Functions and Methods exercise #7

---
+++ PY100/exercises/func_and_method_e7.py
@@ -0,0 +1,7 @@
+# Without running the following code, what do you think it will do?
+
+def foo(bar, qux):
+	print(bar)
+	print(qux)
+
+foo('Hello') # Running this code will cause an error since it is missing the 2nd argument.
