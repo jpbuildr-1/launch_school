287d956a609795d6287b09db4135cf6082e6e860
Jose Alexander Penado <j.penado@outlook.com>
2025-08-31 06:14:48 -0500

    Add answer to Functions and Methods exercise 12

---
+++ PY100/exercises/func_and_method_e12.py
@@ -0,0 +1,8 @@
+# Without running the following code, what do you think it will do?
+
+def foo(first, second=3, third=2):
+	print(first)
+	print(second)
+	print(third)
+
+foo() # Code will give an error type, TypeError, because it is missing a required argument to be passed
