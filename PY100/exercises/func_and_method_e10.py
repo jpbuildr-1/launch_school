09dc7c4e7c5e8762809784a99397dbfe676e04f5
Jose Alexander Penado <j.penado@outlook.com>
2025-08-31 06:09:31 -0500

    Add answer to Functions and Methods exercise 10

---
+++ PY100/exercises/func_and_method_e10.py
@@ -0,0 +1,8 @@
+# Without running the following code, what do you think it will do?
+
+def foo(first, second=3, third=2):
+    print(first)
+	print(second)
+	print(third)
+
+foo(42, 3.141592) # Prints 42\n3.141592\n2
