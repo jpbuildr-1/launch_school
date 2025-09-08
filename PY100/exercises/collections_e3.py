ad16e830d7730dbb5f2997499b805102cf7b0d11
Jose Alexander Penado <j.penado@outlook.com>
2025-09-08 07:00:45 -0500

    Add answer to intro to collections exercise #3

---
+++ PY100/exercises/collections_e3.py
@@ -0,0 +1,14 @@
+"""
+
+Two differences between lists and tuples
+
+1. Tuples are immutable, while lists are mutable.
+2. The syntax to create a list contains [], while tuples contain () and a comma.
+
+
+Two similarities between lists and tuples
+
+1. They are both heterogenous. Which means they can hold any object type.
+2. They are both sequences or ordered collections.
+
+"""
