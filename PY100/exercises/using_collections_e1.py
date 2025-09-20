95ff8b286caf5d62ab8511b37bcb445c5f9ca8ec
Jose Alexander Penado <j.penado@outlook.com>
2025-09-20 09:12:03 -0500

    Add answer to exercise #1 for Using Collections

---
+++ PY100/exercises/using_collections_e1.py
@@ -0,0 +1,13 @@
+# Write Python code to print the seventh number of range(0, 25, 3).
+
+# [0, 3, 6, 9, 12, 15, 18, 21, 24]
+# [0, 1, 2, 3, 4,  5,  6,  7,  8 ]
+# [1, 2, 3, 4, 5,  6,  7,  8,  9 ]
+
+# number_range = list(range(0, 25, 3))
+
+# print(number_range[6])
+
+number_range = range(0, 25, 3)
+
+print(number_range[6])
