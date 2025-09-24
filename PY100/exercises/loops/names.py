95ec1585fcf9db344ca3ab06e5f08ddaab008e26
Jose Alexander Penado <j.penado@outlook.com>
2025-09-24 06:39:04 -0500

    Add for loop example

--- PY100/exercises/loops/names.py
+++ PY100/exercises/loops/names.py
@@ -1,10 +1,17 @@
 names = ['Chris', 'Max', 'Karis', 'Victor']
 upper_names = []
-index = 0
 
-while index < len(names):
-	upper_name = names[index].upper()
+# Uses While Loop
+# index = 0
+
+# while index < len(names):
+# 	upper_name = names[index].upper()
+# 	upper_names.append(upper_name)
+# 	index += 1
+
+# For loop example
+for name in names:
+	upper_name = name.upper()
 	upper_names.append(upper_name)
-	index += 1
 
 print(upper_names) # ['CHRIS', 'MAX', 'KARIS', 'VICTOR']
