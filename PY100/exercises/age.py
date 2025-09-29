cefcfbc66f4d29bc0a882f9601e735b5dd0cfa69
Jose Alexander Penado <j.penado@outlook.com>
2025-09-29 06:55:35 -0500

    Add answer to exercise 2 in Loops and Iterating

--- PY100/exercises/age.py
+++ PY100/exercises/age.py
@@ -9,7 +9,8 @@ Here's the output for someone who is 27 years old.
 age = int(input("How old are you? "))
 print()
 print(f'You are {age} years old.')
-print(f'In 10 years, you will be {age + 10} years old.')
-print(f'In 20 years, you will be {age + 20} years old.')
-print(f'In 30 years, you will be {age + 30} years old.')
-print(f'In 40 years, you will be {age + 40} years old.')
+
+add_to_age = [10, 20, 30, 40]
+
+for number in add_to_age:
+	print(f'In {number} years, you will be {age + number} years old.')
