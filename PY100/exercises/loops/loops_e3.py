6dd0fb521a50d69592e1ef64e9e6fe8406131d24
Jose Alexander Penado <j.penado@outlook.com>
2025-10-07 21:01:28 -0500

    Add answer to loops and iterating exercise 3

---
+++ PY100/exercises/loops/loops_e3.py
@@ -0,0 +1,19 @@
+my_list = [6, 3, 0, 11, 20, 4, 17]
+
+index = 0
+
+# while loop
+while index < len(my_list):
+	print(my_list[index])
+
+	index += 1
+
+	# Uses break
+	# if index == len(my_list):
+	# 	break
+
+# for loop
+for number in my_list:
+	print(number)
+
+# for loop with index
