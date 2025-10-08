a7ad250c5b27ebf1965119064331913119ebc896
Jose Alexander Penado <j.penado@outlook.com>
2025-10-08 15:46:10 -0500

    Add answer to exercise 6 to loops and iterating

---
+++ PY100/exercises/loops/loops_e6.py
@@ -0,0 +1,15 @@
+my_list = [
+	1, 3, 6, 11,
+	4, 2, 4, 9,
+	17, 16, 0,
+]
+
+new_list = [ 'even' if number % 2 == 0 else 'odd' for number in my_list ]
+
+# for number in my_list:
+# 	if number % 2 == 0:
+# 		new_list.append('even')
+# 	else:
+# 		new_list.append('odd')
+
+print(new_list)
