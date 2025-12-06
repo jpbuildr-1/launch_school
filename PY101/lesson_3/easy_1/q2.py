ee2f91bd4860fcacaddb97b2bb7d2636f33fbb5a
Jose Alexander Penado <j.penado@outlook.com>
2025-12-06 07:19:42 -0600

    Add answer to easy 1 q2

---
+++ PY101/lesson_3/easy_1/q2.py
@@ -0,0 +1,11 @@
+"""
+How can you determine whether a given string ends with an exclamation mark(!)?
+Write some code that print `True` or `False` depending on whether the string
+ends with an exclamation mark.
+"""
+
+str1 = "Come over here!" # True
+str2 = "What's up, Doc?" # False
+
+print(str1.endswith('!'))
+print(str2.endswith('!'))
