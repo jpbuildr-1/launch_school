00e96bdf1136d69622cfc6b187634e4366116f76
Jose Alexander Penado <j.penado@outlook.com>
2025-12-06 20:22:25 -0600

    Add answer to easy 1 q6

---
+++ PY101/lesson_3/easy_1/q6.py
@@ -0,0 +1,11 @@
+# Determine whether the name `Dino` apears in the strings below -- check each
+# string separately:
+
+str1 = "Few things in life are as important as house training your pet dinosaur."
+str2 = "Fred and Wilma have a pet dinosaur named Dino."
+
+print(str1.find('dino'))
+print(str2.find('dino'))
+
+print('dino' in str1)
+print('dino' in str2)
