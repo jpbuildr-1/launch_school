07374a611f65f37d2a284b31be7645efb21f196f
Jose Alexander Penado <j.penado@outlook.com>
2025-12-12 11:19:11 -0600

    Add answer to q10 easy1

---
+++ PY101/lesson_3/easy_1/q10.py
@@ -0,0 +1,4 @@
+# Print the folliwng string the word `important` replaced by `urgent`.
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+print(advice.replace("important", "urgent"))
