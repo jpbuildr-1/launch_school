b1a6e0e47f9c5620574a308896f4ada25b2a654c
Jose Alexander Penado <j.penado@outlook.com>
2025-12-13 17:48:01 -0600

    Add answer to easy 3 q3

---
+++ PY101/lesson_3/easy_3/q3.py
@@ -0,0 +1,5 @@
+# What will the following code output?
+str1 = "hello there"        # str1 = "hello there"
+str2 = str1                 # str1 = "hello there" str2 = "hello there"
+str2 = "goodbye!"           # str1 = "hello there" str2 = "goodbye!"
+print(str1)                 # "hello there"
