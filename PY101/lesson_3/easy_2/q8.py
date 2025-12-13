a7c39d1420596864bcae73162445862041299759
Jose Alexander Penado <j.penado@outlook.com>
2025-12-13 12:09:19 -0600

    Add answer to easy 2 q8

---
+++ PY101/lesson_3/easy_2/q8.py
@@ -0,0 +1,6 @@
+# Determine whether the following dictionary of people and their age
+# contains any entry for 'Spot':
+
+ages = {'Herman': 32, 'Lily': 30, 'Grandpa': 402, 'Eddie': 10}
+
+'Spot' in ages
