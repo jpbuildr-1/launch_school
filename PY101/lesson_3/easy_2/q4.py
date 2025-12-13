f48de532f6c4aed2c3cb953b01e1efd5af70cbd6
Jose Alexander Penado <j.penado@outlook.com>
2025-12-13 11:27:42 -0600

    Add answer to easy 2 q4

---
+++ PY101/lesson_3/easy_2/q4.py
@@ -0,0 +1,8 @@
+# Given a list of numbers [1, 2, 3, 4, 5], mutate the list by removing the number at index `2`, so that
+# the list become [1, 2, 4, 5].
+
+numbers = [1, 2, 3, 4, 5]
+# numbers.pop(2)
+del numbers[2]
+numbers
+print(numbers)
