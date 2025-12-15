0df734f0b5d2a28df3a820eca3c49d5a38a85f69
Jose Alexander Penado <j.penado@outlook.com>
2025-12-15 06:59:33 -0600

    Add answer to medium 1 q4

---
+++ PY101/lesson_3/medium_1/q4.py
@@ -0,0 +1,4 @@
+# What will the following two lines of code output?
+print(0.3 + 0.6)         # 0.8999999999
+print(0.3 + 0.6 == 0.9)  # False as 0.3 + 0.6 evaulate to 0.8999999 and is not equivalent to 0.9
+# if you use math.isclose(0.3 + 0.6, 0.9) will return True
