bcc8e4be77406a8aa4190f7cb29d0754988e313e
Jose Alexander Penado <j.penado@outlook.com>
2026-04-03 14:22:11 -0500

    Initial twenty one implementation

--- PY110/lesson_3/twenty_one.py
+++ PY110/lesson_3/twenty_one.py
@@ -28,7 +28,7 @@
     - If the player card value is greater than 21 then bust and game ends.
 5. Dealer turn: hit or stay
     - repeat until total >= 17
-        - Randomly deal one card to player from the available deck, for each card dealt add card value to the player value and set the bool in the deck to True
+        - Randomly deal one card to dealer from the available deck, for each card dealt add card value to the dealer value and set the bool in the deck to True
         - End dealer turn when total is greater than or equal to 17
 6. If dealer busts, player wins.
     - If the dealer card value is greater than 21 then bust and game ends.
@@ -38,16 +38,20 @@
     - Game is tied if card values are equal
 '''
 import random
+import os
+
+def prompt(phrase):
+    print(f'===> {phrase}')
 
 def initialize_deck():
     suits = ['spades', 'clubs', 'hearts', 'diamonds']
-    card = {'ace': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10, 'jack': 10, 'queen': 10, 'king': 10}
-    return {f"{name} of {suit}": [card[name], False] for suit in suits
+    card = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'jack': 10, 'queen': 10, 'king': 10, 'ace': [1, 11]}
+    return {f"{name} of {suit}": [card[name], name, suit, False] for suit in suits
                       for name in card}
 
 def cards_available(deck):
-    return [name for name in deck
-                 if deck[name][1] == False]
+    return [card for card in deck
+                 if deck[card][3] == False]
 
 def deal_hand(deck):
     hand = deal_card(deck) + deal_card(deck)
@@ -55,13 +59,65 @@ def deal_hand(deck):
 
 def deal_card(deck):
     card = random.choice(cards_available(deck))
-    deck[card][1] = True
+    deck[card][3] = True
     return [card]
+
+def ace_check(deck, hand):
+    ['ace of spades', 'ace of clubs', 'ace of hearts', 'ace of diamonds']
+
+
+def display_board(deck, dealer_hand, player_hand):
+    os.system('clear')
+    prompt(f"Dealer has: {deck[dealer_hand[0]][1]} and unknown card")
+    prompt(f"You have: {' and '.join([deck[card][1] for card in player_hand])}")
+    prompt(total_hand(deck, player_hand))
+
+
+def total_hand(deck, hand):
+    total = 0
+    ace_count = 0
+    for card in hand:
+        if deck[card][1] == 'ace':
+            ace_count += 1
+        else:
+            total += deck[card][0]
+    if ace_count == 1:
+        if total + 11 > 21:
+            total = total + 1
+        else:
+            total = total + 11
+    elif ace_count == 2:
+        if total + 11 > 21:
+            total = total + 1 + 1
+        else:
+            total = total + 11 + 1
+    return total
 
 def twenty_one():
     deck = initialize_deck()
-    player_hand = deal_hand(deck)
     dealer_hand = deal_hand(deck)
+    player_hand = deal_hand(deck)
+
+    display_board(deck, dealer_hand, player_hand)
+
+    prompt("Would you want to hit or stay?")
+    hit_or_stay = input()
+
+    while total_hand(deck, player_hand) < 21 and hit_or_stay == 'hit':
+        player_hand += deal_card(deck)
+        display_board(deck, dealer_hand, player_hand)
+        if total_hand(deck, player_hand) > 21:
+            prompt('Bust, dealer wins.')
+            break
+        prompt("Would you want to hit or stay?")
+        hit_or_stay = input()
+    while total_hand(deck, dealer_hand) < 18:
+        dealer_hand += deal_card(deck)
+        if total_hand(deck, dealer_hand) > 21:
+            prompt('Dealer busts, player wins.')
+            break
+    display_board(deck, dealer_hand, player_hand)
+    
 
 
 twenty_one()
