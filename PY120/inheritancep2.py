277cd8befc597da14ac50a872d13f75bc3f2ebb4
Jose Alexander Penado <j.penado@outlook.com>
2026-06-03 21:28:17 -0500

    Inheritance excercise

---
+++ PY120/inheritancep2.py
@@ -0,0 +1,35 @@
+class Vehicle:
+    
+    count = 0
+    
+    def __init__(self):
+        Vehicle.count += 1
+
+    @classmethod
+    def vehicles(cls):
+        return Vehicle.count
+
+class Car(Vehicle):
+    pass
+
+
+class Truck(Vehicle):
+    pass
+
+class Boat(Vehicle):
+    pass
+
+
+print(Car.vehicles())     # 0
+car1 = Car()
+print(Car.vehicles())     # 1
+car2 = Car()
+car3 = Car()
+car4 = Car()
+print(Car.vehicles())     # 4
+truck1 = Truck()
+truck2 = Truck()
+print(Truck.vehicles())   # 6
+boat1 = Boat()
+boat2 = Boat()
+print(Boat.vehicles())    # 8
