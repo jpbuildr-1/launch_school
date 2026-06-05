6af1fb13e54e153cda5a8ff7dbf78bcad9a4be1e
Jose Alexander Penado <j.penado@outlook.com>
2026-06-05 13:58:51 -0500

    Problem Sets 1

--- PY120/problemsets.py
+++ PY120/problemsets.py
@@ -18,14 +18,16 @@ bob.name = 'Robert'
 print(bob.name)           # Robert
 
 '''
-line 15 the local variable bob is assigned a Person object with the instance variable name of 'bob'. What happens is the Person class
-is invoked and a new instance in created using the `__init__` method which passes 'bob' and assigns it to an instance variable.
+On line 15 a Person object is initialized with 'bob' as an argument. During initialization, the instance setter property `name` is invoked
+and sets the instance variable `_name` to 'bob' for this specific instance of the Person class.
 
-line 16 outputs the name instance variable 'bob' via calling the behavior that gets the name instance variable 'bob' from the Person object that is
-assigned to the local variable bob.
+line 16 outputs bob via the instance get property `name` which return the instance variable `_name`. The instance variable `_name`
+of the Person instance assigned to bob is 'bob' so when the `name` property is invoked on bob it will return 'bob' which then is
+passed to the print invocation that then outputs bob.
 
-line 17 sets the name instance variable assigned to the local variable bob from 'bob' to 'Robert'
+line 17 invokes the instance setter property `name` on bob which is assigned a Person instance that contain the instance variable 'bob'.
+The instance property setter updates the `_name` instance variable from 'bob' to 'Robert'.
 
-line 18 out puts the name instance variable 'Robert' via calling the behavior that gets the name instance variable 'Robert' from the Person
-object that is assigned to the local variable bob.
+line 18 outputs Robert via the instance get property that is invoked on the Person instance assigned to bob. The `name` property returns
+the instance variable `_name` assigned to bob which is 'Robert` and then the print invocation outputs Robert.
 '''
