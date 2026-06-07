0c60f2dbdb1d9462a269e27bf696001384ad2519
Jose Alexander Penado <j.penado@outlook.com>
2026-06-06 20:42:51 -0500

    Classes and Objects exercises

---
+++ PY120/create_the_object.py
@@ -0,0 +1,10 @@
+# Using the code from the previous exercise, create an instance of Cat and assign it to a variable named kitty
+
+class Cat:
+    pass
+
+kitty = Cat()
+
+'''
+On line 6 the Cat class instantiates a new Cat instance and assigns it to the variable kitty
+'''
