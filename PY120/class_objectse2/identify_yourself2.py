eb1b64602830a1891e5ed6f4942b4ca2e27bdf12
Jose Alexander Penado <j.penado@outlook.com>
2026-06-07 22:03:40 -0500

    Classes and objects exercise

---
+++ PY120/class_objectse2/identify_yourself2.py
@@ -0,0 +1,13 @@
+class Cat:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    def __str__(self):
+        return f"I'm {self.name}!"
+
+kitty = Cat('Sophie')
+print(kitty)        # I'm Sophie!
