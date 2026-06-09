6b5358a150e220aa1ea3a88207504bce8b41a490
Jose Alexander Penado <j.penado@outlook.com>
2026-06-08 20:34:30 -0500

    Inheritance and Mixins exercise

---
+++ PY120/inheritancee1/towing_mixin2.py
@@ -0,0 +1,20 @@
+class TowingMixin:
+    def tow(self):
+        return 'I can tow a trailer!'
+
+class Vehicle:
+    def __init__(self, year):
+        self.year = year
+
+class Truck(Vehicle, TowingMixin):
+    pass
+
+class Car(Vehicle):
+    pass
+
+truck1 = Truck(1994)
+print(truck1.year)          # 1994
+print(truck1.tow())         # I can tow a trailer!
+
+car1 = Car(2006)
+print(car1.year)            # 2006
