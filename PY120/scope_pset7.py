551c00c126acab24f8b2da6c207e60094134c57a
Jose Alexander Penado <j.penado@outlook.com>
2026-07-05 17:46:16 -0500

    Add answer to scope problem 7

---
+++ PY120/scope_pset7.py
@@ -0,0 +1,12 @@
+class Student:
+    school_name = 'Oxford'
+
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def get_school_name(cls):
+        return cls.school_name
+
+print(Student.get_school_name())    # Oxford
+print(Student.school_name)          # Oxford
