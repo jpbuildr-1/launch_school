223545ee324625b806ea79476b1af22c964d6198
Jose Alexander Penado <j.penado@outlook.com>
2026-07-07 22:16:06 -0500

    Add answer to exceptions problem 7

---
+++ PY120/exceptions_pset7.py
@@ -0,0 +1,6 @@
+'''
+a) 5 / 2            # outputs a 2.5
+b) 3 // 0           # raises a ZeroDivisionError
+c) 8 % (1 - 1)      # raises a ZeroDivisionError
+d) 7 / (3 + 4)      # outputs a 1.0
+'''
