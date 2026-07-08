cc190336ae4742f4d0c517dc9c8752ac31795d5a
Jose Alexander Penado <j.penado@outlook.com>
2026-07-07 22:29:20 -0500

    Add answer to excpetions problem 10

---
+++ PY120/exceptions_pset10.py
@@ -0,0 +1,6 @@
+'''
+a) 'hello'.upper()                  # returns 'HELLO'
+b) [1, 2, 3].push(4)                # raises AttributeError
+c) {'key': 'value'}.get('key')      # returns 'value'
+d) (12345).length()                 # raises AttributeError
+'''
