dd5595035247ba8fddca140eda7b634d4d2110db
Jose Alexander Penado <j.penado@outlook.com>
2026-07-09 12:21:59 -0500

    Add answer to easy 1 problem 3

---
+++ PY120/practice_e1.3.py
@@ -0,0 +1,14 @@
+class SpeedMixin:
+    def go_fast(self):
+        print(f'I am a super fast {self.__class__.__name__}')
+
+class Car(SpeedMixin):
+    def go_slow(self):
+        print('I am safe and driving slow.')
+
+class Truck(SpeedMixin):
+    def go_very_slow(self):
+        print('I am a heavy truck and like going very slow.')
+
+Truck().go_fast()     # I am a super fast Truck
+Car().go_fast()       # I am a super fast Car
