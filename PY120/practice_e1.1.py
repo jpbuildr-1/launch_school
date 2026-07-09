315bea48c9adb5f1d7a4a77b3cd911ed6dd75a95
Jose Alexander Penado <j.penado@outlook.com>
2026-07-09 12:10:33 -0500

    Add answer to easy1 problem 1

---
+++ PY120/practice_e1.1.py
@@ -0,0 +1,12 @@
+'''
+`True_`
+`'hello'`
+`[1, 2, 3, 'happy days']`
+`142`
+`{1, 2, 3}`
+`1.2345`
+
+They are all objects. You can use `.__class__` or `.__class__.__name__` to 
+see what class they belong to.
+
+'''
