1ec52ad401b35e74cad626b9b5f197239922b77e
Jose Alexander Penado <j.penado@outlook.com>
2026-07-11 12:30:10 -0500

    Answer oop easy 2 problem 5

---
+++ PY120/practice_e2.5.py
@@ -0,0 +1,17 @@
+class Greeting:
+    def greet(self, message):
+        print(message)
+
+class Hello(Greeting):
+    def hi(self):
+        self.greet('Hello')
+
+    @classmethod
+    def hi(cls):
+        Greeting().greet('Hi')
+
+class Goodbye(Greeting):
+    def bye(self):
+        self.greet('Goodbye')
+
+Hello.hi()      # Hi
