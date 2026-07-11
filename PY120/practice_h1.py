69371f36154580a8e8b543ea0a1ec4adce9f03e9
Jose Alexander Penado <j.penado@outlook.com>
2026-07-11 14:34:49 -0500

    Answer oop hard problem 1

---
+++ PY120/practice_h1.py
@@ -0,0 +1,58 @@
+class FuelMixin:
+    def set_fuel(self, kilometers_per_liter, liters_of_fuel_capacity):
+        self.fuel_efficiency = kilometers_per_liter
+        self.fuel_capacity = liters_of_fuel_capacity
+
+    def range(self):
+        return self.fuel_capacity * self.fuel_efficiency
+
+class WheeledVehicle(FuelMixin):
+    def __init__(self,
+                 tire_list,
+                 kilometers_per_liter,
+                 liters_of_fuel_capacity):
+        self.tires = tire_list
+        self.set_fuel(kilometers_per_liter, liters_of_fuel_capacity)
+
+    def tire_pressure(self, tire_index):
+        return self.tires[tire_index]
+
+    def inflate_tire(self, tire_index, pressure):
+        self.tires[tire_index] = pressure
+
+
+class Auto(WheeledVehicle):
+    def __init__(self):
+        # 4 tires with various tire pressures
+        super().__init__([30, 30, 32, 32], 50, 25.0)
+
+class Motorcycle(WheeledVehicle):
+    def __init__(self):
+        # 2 tires with various tire pressures
+        super().__init__([20, 20], 80, 8.0)
+
+class Catamaran(FuelMixin):
+    def __init__(self,
+                 number_propellers,
+                 number_hulls,
+                 kilometers_per_liter,
+                 liters_of_fuel_capacity):
+        self.propellers = number_propellers
+        self.hulls = number_hulls
+        self.set_fuel(kilometers_per_liter, liters_of_fuel_capacity)
+
+auto = Auto()
+motorcycle = Motorcycle()
+catamaran = Catamaran(2, 2, 1.5, 600)
+
+print(auto.fuel_efficiency)             # 50
+print(auto.fuel_capacity)               # 25.0
+print(auto.range())                     # 1250.0
+
+print(motorcycle.fuel_efficiency)       # 80
+print(motorcycle.fuel_capacity)         # 8.0
+print(motorcycle.range())               # 640.0
+
+print(catamaran.fuel_efficiency)        # 1.5
+print(catamaran.fuel_capacity)          # 600
+print(catamaran.range())                # 900.0
