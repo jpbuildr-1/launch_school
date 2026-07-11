906e3e5fc1bfd355c1f176d2269593b4fbdd14a5
Jose Alexander Penado <j.penado@outlook.com>
2026-07-11 12:07:17 -0500

    Add answer to oop easy 2 problem 3

---
+++ PY120/practice_e2.3.py
@@ -0,0 +1,5 @@
+'''
+Easier to organize code using nouns and verbs
+Easier to maintain complex and ever increasing amount of code
+Easier to reason the logic around what each class does in the program
+'''
