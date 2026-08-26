8b4ba56e11e6cd1c80d6ac7c1215f909281eb42c
Jose Alexander Penado <j.penado@outlook.com>
2026-08-26 12:18:53 -0500

    Add answer to generator practice problem 1

---
+++ PY130/practice_problems/generator1.py
@@ -0,0 +1,9 @@
+'''
+Create a generator expression that generates the reciprocals of the numbers from 1 to 10. 
+A reciprocal of a number n is 1 / n. Use a for loop to print each value.
+'''
+
+reciprocals = (1 / number for number in range(1, 11))
+
+for reciprocal in reciprocals:
+    print(reciprocal)
