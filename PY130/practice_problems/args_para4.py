690444c4234870a1cd6b9cdc136471ed58c40191
Jose Alexander Penado <j.penado@outlook.com>
2026-08-27 16:54:52 -0500

    Add answer to args and para problem 4

---
+++ PY130/practice_problems/args_para4.py
@@ -0,0 +1,5 @@
+def calculate_average(*args):
+    return sum(args) / len(args) if args else None
+
+print(calculate_average(1, 2, 3, 4, 5))      # 3.0
+print(calculate_average())                   # None
