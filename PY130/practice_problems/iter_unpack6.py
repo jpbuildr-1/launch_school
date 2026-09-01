acbadf32a1bca89fc5049218cfb29ec4412edb8f
Jose Alexander Penado <j.penado@outlook.com>
2026-08-31 21:43:13 -0500

    Add answer to iterable unpacking problem 6

---
+++ PY130/practice_problems/iter_unpack6.py
@@ -0,0 +1,9 @@
+first, *middle, last = "hello"
+print(f"First: {first}, Middle: {middle}, Last: {last}")
+
+# The code will output is
+# First: h, Middle: ['e', 'l', 'l'], Last: o
+# The `first` element would contain the first element of the string "hello"
+# The `last` element would contain the last element of the string "hello"
+# The `middle` element would contain the elements between first and last
+# in a list
