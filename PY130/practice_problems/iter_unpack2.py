f812be0fe718f5ab034c02e14cc69133cadcac82
Jose Alexander Penado <j.penado@outlook.com>
2026-08-31 21:25:08 -0500

    Add answer to iterable unpacking problem 2

---
+++ PY130/practice_problems/iter_unpack2.py
@@ -0,0 +1,5 @@
+a, _, c = (1, 2, 3)
+
+# The underscore `_` is assigned the value `2`. In Python `_` is used as a
+# throwaway variable. In this example the second value of the tuple is
+# assigned to the `_`
