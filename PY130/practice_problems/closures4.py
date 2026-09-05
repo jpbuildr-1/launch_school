c1efb272fba2fc60414ad2bc971ff93ffcf06d7f
Jose Alexander Penado <j.penado@outlook.com>
2026-09-05 10:33:07 -0500

    Add answer to closure problem 4

---
+++ PY130/practice_problems/closures4.py
@@ -0,0 +1,17 @@
+'''
+Write a function named later that takes two arguments: a function, func, and an argument for 
+that function, argument. The return value should be a new function that calls func with argument 
+as its argument. Here's an example of how it might be used:
+'''
+
+def later(func, arg):
+    def run_it():
+        func(arg)
+    
+    return run_it
+
+def printer(message):
+    print(message)
+
+print_warning = later(printer, "The system is shutting down!") # returns the function printer, and saves the argument
+print_warning()  # The system is shutting down!
