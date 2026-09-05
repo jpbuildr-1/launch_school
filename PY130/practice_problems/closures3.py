5bee84d95681f9b464b78cbada4888ce3f10bbd1
Jose Alexander Penado <j.penado@outlook.com>
2026-09-05 10:20:52 -0500

    Add answer to closures problem 3

---
+++ PY130/practice_problems/closures3.py
@@ -0,0 +1,12 @@
+'''
+What will this code print? Try to answer without running the code:
+'''
+
+from functools import partial
+
+def greet(name, greeting):
+    return f"{greeting}, {name}!"
+
+say_hello_to = partial(greet, greeting="Hello")
+print(say_hello_to(name="Alice"))  # What will this print?
+# Hello, Alice!
